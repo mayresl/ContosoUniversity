39ee642e72de1183b62c6e1b4f4592b614ffbc8e
Marcela Ayres Leite <mayresl_292@hotmail.com>
2018-03-18 17:45:17 -0300

    Done: Razor Pages with Entity Framework Core in ASP.NET Core - Tutorial 1 of 8

---
+++ ContosoUniversity/Data/SchoolContext.cs
@@ -0,0 +1,33 @@
+﻿using ContosoUniversity.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoUniversity.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
+        {
+        }
+
+        /*
+             DbSet<Enrollment> and DbSet<Course> can be omitted. 
+             EF Core includes them implicitly because the Student entity references the Enrollment entity, 
+             and the Enrollment entity references the Course entity.
+        */
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+        /*
+             Since EF Core creates tables that have names the same as the DbSet property names and property names 
+             for collections are typically plural, you can use the following code to specify singular table names
+             (developers disagree about whether table names should be plural)
+        */
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Student>().ToTable("Student");
+        }
+    }
+}
---
+++ ContosoUniversity/Models/Course.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public class Course
+    {
+        //This attribute allows the app to specify the primary key rather than having the DB generate it.
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
